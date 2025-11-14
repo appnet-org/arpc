0f197da444c02215badef63fd2b44d6ebd129885
Zhuo Chen <zhuochen0624@gmail.com>
2025-11-14 18:13:27 -0500

    refactor: optimize reliable transport.

--- pkg/custom/reliable/client_handler.go
+++ pkg/custom/reliable/client_handler.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/appnet-org/arpc/pkg/logging"
 	"github.com/appnet-org/arpc/pkg/packet"
-	"github.com/appnet-org/arpc/pkg/transport"
 	"go.uber.org/zap"
 )
 
@@ -25,7 +24,15 @@ func NewReliableClientHandlerWithTimeout(transportSender TransportSender, timerM
 	handler := &ReliableClientHandler{
 		ReliableHandler: newReliableHandler(transportSender, timerMgr, timeout),
 	}
-	handler.startCleanupTimer(transport.TimerKey("reliable_client_cleanup"))
+
+	// Cache ACK packet type during initialization
+	ackPacketType, exists := transportSender.GetPacketRegistry().GetPacketTypeByName(AckPacketName)
+	if !exists {
+		logging.Fatal("ACK packet type not registered in transport - ensure ACK packet type is registered before creating reliable handler")
+	}
+	handler.ackPacketType = &ackPacketType
+
+	handler.startCleanupTimer(TimerKeyReliableClientCleanup)
 
 	logging.Debug("Reliable client handler created",
 		zap.Duration("timeout", timeout))
@@ -85,5 +92,5 @@ func (h *ReliableClientHandler) handleReceiveACK(ack *ACKPacket, addr *net.UDPAd
 
 // Cleanup cleans up resources
 func (h *ReliableClientHandler) Cleanup() {
-	h.ReliableHandler.Cleanup(transport.TimerKey("reliable_client_cleanup"))
+	h.ReliableHandler.Cleanup(TimerKeyReliableClientCleanup)
 }
